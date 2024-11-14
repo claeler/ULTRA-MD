2dcbcba2d861ca0a0df790a1fdb207f428001c76
red room <119407515+claeler@users.noreply.github.com>
2024-11-14 12:08:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '201143539799';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
